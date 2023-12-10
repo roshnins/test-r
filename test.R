04e3427cec4e039c5188f6e347350b3c3d649ee7
roshnins <roshnins@hotmail.com>
2023-12-11 00:25:58 +0800

    penguins ggplot 1

--- test.R
+++ test.R
@@ -1,1 +1,45 @@
-a <- 1
+install.packages("tidyverse")
+install.packages(
+  c("arrow", "babynames", "curl", "duckdb", "gapminder", 
+    "ggrepel", "ggridges", "ggthemes", "hexbin", "janitor", "Lahman", 
+    "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins", 
+    "repurrrsive", "tidymodels", "writexl")
+)
+library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
+
+penguins
+#> # A tibble: 344 × 8
+#>   species island    bill_length_mm bill_depth_mm flipper_length_mm
+#>   <fct>   <fct>              <dbl>         <dbl>             <int>
+#> 1 Adelie  Torgersen           39.1          18.7               181
+#> 2 Adelie  Torgersen           39.5          17.4               186
+#> 3 Adelie  Torgersen           40.3          18                 195
+#> 4 Adelie  Torgersen           NA            NA                  NA
+#> 5 Adelie  Torgersen           36.7          19.3               193
+#> 6 Adelie  Torgersen           39.3          20.6               190
+#> # ℹ 338 more rows
+#> # ℹ 3 more variables: body_mass_g <int>, sex <fct>, year <int>
+
+glimpse(penguins)
+#> Rows: 344
+#> Columns: 8
+#> $ species           <fct> Adelie, Adelie, Adelie, Adelie, Adelie, Adelie, A…
+#> $ island            <fct> Torgersen, Torgersen, Torgersen, Torgersen, Torge…
+#> $ bill_length_mm    <dbl> 39.1, 39.5, 40.3, NA, 36.7, 39.3, 38.9, 39.2, 34.…
+#> $ bill_depth_mm     <dbl> 18.7, 17.4, 18.0, NA, 19.3, 20.6, 17.8, 19.6, 18.…
+#> $ flipper_length_mm <int> 181, 186, 195, NA, 193, 190, 181, 195, 193, 190, …
+#> $ body_mass_g       <int> 3750, 3800, 3250, NA, 3450, 3650, 3625, 4675, 347…
+#> $ sex               <fct> male, female, female, NA, female, male, female, m…
+#> $ year              <int> 2007, 2007, 2007, 2007, 2007, 2007, 2007, 2007, 2…
+
+View(penguins) 
+# opens a new tab of spreadsheet
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point()
+#> Warning: Removed 2 rows containing missing values (`geom_point()`).
